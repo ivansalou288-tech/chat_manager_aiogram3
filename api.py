133f43dd81725ee19411ac1dec1f81e39d73050d
Ноутбук Ezhik <ezhik@gmail.com>
2026-02-26 12:57:13 +0300

    api

--- api.py
+++ api.py
@@ -238,8 +238,26 @@ def get_recom(user: int):
     connection = sqlite3.connect(main_path, check_same_thread=False)
     cursor = connection.cursor()
     all = cursor.execute('SELECT * FROM recommendation WHERE user_id = ?', (user,)).fetchall()
-    print(all)
-    return all
+    recomendations = []
+    for rec in all:
+        print(rec)
+        id = rec[0]
+        pubg_id = rec[1]
+        moder_id = rec[2]
+        reason = rec[3]
+        rang = rec[4]
+        date = rec[5]
+        rec_id = rec[6]
+        recom = {"id": id,
+                 "pubg_id": pubg_id,
+                 "moder_id": moder_id,
+                 "reason": reason,
+                 "rang": rang,
+                 "date": date,
+                 "rec_id": rec_id,
+                 }
+        recomendations.append(recom)
+    return recomendations
 
 @app.post("/ban")
 def ban(action: BanAction):
