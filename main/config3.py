956b7a5f7b74f3c6906db9b78ecea574d8656a46
Ноутбук Ezhik <ezhik@gmail.com>
2026-03-01 13:47:13 +0300

    main

--- main/config3.py
+++ main/config3.py
@@ -364,6 +364,8 @@ async def recom_check_sdk(tg_id, name_user):
             name_mod = cursor.execute(f"SELECT nik FROM [{-(sost_1)}] WHERE tg_id=?", (int(moder_gives[i]),)).fetchall()[0][0]
         except IndexError:
             name_mod = moder_gives[i]
+        except ValueError:
+            name_mod = moder_gives[i]
         textt = f'🟢 <b>{i+1}</b>. От <a href="tg://user?id={moder_gives[i]}">{name_mod}</a> | Должность: <b>{moder_rang[i]}</b>\n<b>&#8195Чем отличился:</b> {comments[i]}\n<b>&#8195Рекомендован на:</b> {rang[i]}\n<b>&#8195Дата рекомендации: {date[i]}</b>'
         itog.append(textt)
     text = '\n\n'.join(itog)
